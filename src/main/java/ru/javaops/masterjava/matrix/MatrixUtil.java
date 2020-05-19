6af023d977c2000f18fb6005381d557d57d2ba78
Maxim Rodionov <rodionov.m.dev@gmail.com>
2020-05-19 10:23:02 +0300

    HW1 init commit

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,7 @@
 package ru.javaops.masterjava.matrix;
 
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
 
 /**
  * gkislin
@@ -12,10 +11,42 @@ public class MatrixUtil {
 
     // TODO implement parallel multiplication matrixA*matrixB
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
-        final int matrixSize = matrixA.length;
-        final int[][] matrixC = new int[matrixSize][matrixSize];
+        int size = matrixA.length;
+        CompletionService<Void> service = new ExecutorCompletionService<>(executor);
+        CountDownLatch latch = new CountDownLatch(size);
 
+        int[][] matrixC = new int[size][size];
+
+        int[][] transposeB = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                transposeB[i][j] = matrixB[j][i];
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            int I = i;
+            service.submit(() -> {
+                calcRow(I, matrixA, transposeB, matrixC);
+                latch.countDown();
+            }, null);
+        }
+        latch.await();
         return matrixC;
+    }
+
+     static void calcRow(int row, int[][] matrixA, int[][] matrixB, int[][] matrixC) {
+        int size = matrixA.length;
+        for(int i = 0; i < size; i++) {
+            int sum = 0;
+            int[] rowA = matrixA[row];
+            int[] colB = matrixB[i];
+            for(int j = 0; j < size; j++) {
+                sum += rowA[j] * colB[j];
+            }
+            matrixC[row][i] = sum;
+        }
     }
 
     // TODO optimize by https://habrahabr.ru/post/114797/
@@ -23,13 +54,19 @@ public class MatrixUtil {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+        final int[] transposeColumn = new int[matrixSize];
+
         for (int i = 0; i < matrixSize; i++) {
+            for(int k = 0; k < matrixSize; k++) {
+                transposeColumn[k] = matrixB[k][i];
+            }
+
             for (int j = 0; j < matrixSize; j++) {
                 int sum = 0;
                 for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+                    sum += matrixA[j][k] * transposeColumn[k];
                 }
-                matrixC[i][j] = sum;
+                matrixC[j][i] = sum;
             }
         }
         return matrixC;
